34bc2b59a51e5e46fcfeced787beb13de8f749ac
Uhaytori <samsherrawal@yahoo.com>
2023-08-16 12:03:36 -0500

    removed comments in Settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -64,27 +64,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'taskmate.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'march1306',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-# CREATING A PRODUCTION SERVER CONFIGURATION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -95,9 +74,6 @@ DATABASES = {
         'PORT': env("DJANGO_DB_PORT"),
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
